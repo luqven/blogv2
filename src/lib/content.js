17af94b2ec9f9ee9727603820d696b9aa8d37749
Luis Ball <luqven@gmail.com>
2023-01-31 23:09:03 -0800

    chore: update utterances theme

--- src/lib/content.js
+++ src/lib/content.js
@@ -20,8 +20,8 @@ import remarkGithub from 'remark-github';
 import remarkGfm from 'remark-gfm';
 const remarkPlugins = [
 	remarkToc,
-	[remarkGithub, { repository: 'https://github.com/sw-yx/swyxkit/' }],
-	[remarkGfm, { repository: 'https://github.com/sw-yx/swyxkit/' }]
+	[remarkGithub, { repository: 'https://github.com/luqven/blogv2/' }],
+	[remarkGfm, { repository: 'https://github.com/luqven/blogv2/' }]
 ];
 const rehypePlugins = [
 	rehypeStringify,
